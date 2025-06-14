63c61b8f3cae0e4c1bcea87f899ca0886399e7bc
camilaosternomendes <168308212+camilaosternomendes@users.noreply.github.com>
2025-06-14 18:46:24 -0300

    imports

--- src/main/java/com/busca/model/Artigo.java
+++ src/main/java/com/busca/model/Artigo.java
@@ -1,1 +1,7 @@
+package com.busca.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
