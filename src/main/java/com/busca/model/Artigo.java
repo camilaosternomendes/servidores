89460bd13cc371f6a8ba22cd125bf84775cd4872
camilaosternomendes <168308212+camilaosternomendes@users.noreply.github.com>
2025-06-14 18:46:44 -0300

    novo import

--- src/main/java/com/busca/model/Artigo.java
+++ src/main/java/com/busca/model/Artigo.java
@@ -5,3 +5,4 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.Serializable; 
